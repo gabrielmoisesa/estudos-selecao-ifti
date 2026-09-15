5a6ac98782d302390aad67e104492662ac3b2755
Gabriel Alves <gabrielmoisesalves18@gmail.com>
2026-09-15 09:31:55 +0000

    feat(sql-aula1): style ex1 formatting space style

--- modulo-sql-basico/aula1-sql-basico-consultas/exercicio_01_expandindo_o_banco.sql
+++ modulo-sql-basico/aula1-sql-basico-consultas/exercicio_01_expandindo_o_banco.sql
@@ -3,20 +3,20 @@ GO
 
 -- req 1.
 CREATE TABLE [dbo].[Promocao] (
-  Id INT IDENTITY,
-  IdFilme INT NOT NULL,
-  Descricao VARCHAR(300) NOT NULL,
-  PercentualDesconto DECIMAL(5,2) NOT NULL,
-  DataInicioVigencia DATE NOT NULL,
-  DataFimVigencia DATE NOT NULL,
-  CONSTRAINT PK_Promocao PRIMARY KEY (Id),
-  CONSTRAINT FK_IdFilme_Promocao FOREIGN KEY (IdFilme) REFERENCES [dbo].[Filme] (Id)
+    Id INT IDENTITY,
+    IdFilme INT NOT NULL,
+    Descricao VARCHAR(300) NOT NULL,
+    PercentualDesconto DECIMAL(5,2) NOT NULL,
+    DataInicioVigencia DATE NOT NULL,
+    DataFimVigencia DATE NOT NULL,
+    CONSTRAINT PK_Promocao PRIMARY KEY (Id),
+    CONSTRAINT FK_IdFilme_Promocao FOREIGN KEY (IdFilme) REFERENCES [dbo].[Filme] (Id)
 );
 GO
 
 -- req 2.
 ALTER TABLE [dbo].[Cliente]
-  ADD Telefone VARCHAR(20) NULL;
+    ADD Telefone VARCHAR(20) NULL;
 GO
 
 -- req 3.
@@ -28,20 +28,20 @@ GO
 
 -- req 4.
 UPDATE [dbo].[Cliente]
-  SET Telefone = '11999999999'
-  WHERE Id = 1
+    SET Telefone = '11999999999'
+    WHERE Id = 1
 GO
 
 -- req 5.
 DELETE
-  FROM [dbo].[Promocao]
-  WHERE Id = 3
+    FROM [dbo].[Promocao]
+    WHERE Id = 3
 GO
 
 -- req 6.
-SELECT fi.Titulo as Titulo,
-       pr.Descricao as Descricao,
-       pr.PercentualDesconto as PercentualDesconto
-  FROM [dbo].[Promocao] AS pr WITH(NOLOCK)
-  INNER JOIN [dbo].[Filme] AS fi WITH(NOLOCK) ON fi.Id = pr.IdFilme;
+SELECT  fi.Titulo as Titulo,
+        pr.Descricao as Descricao,
+        pr.PercentualDesconto as PercentualDesconto
+    FROM [dbo].[Promocao] AS pr WITH(NOLOCK)
+    INNER JOIN [dbo].[Filme] AS fi WITH(NOLOCK) ON fi.Id = pr.IdFilme;
 GO
