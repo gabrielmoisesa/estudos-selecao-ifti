fd75263925b5a0832992a5b930cd18ba4ad88b0c
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2026-09-17 03:03:50 -0300

    feat(csharp-aula4): ex2

---
+++ modulo-csharp-basico/aula4-loops-e-relatorios-estatisticos/Exercicio2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Globalization;
+
+CultureInfo culture = new("pt-BR");
+Thread.CurrentThread.CurrentCulture = culture;
+
+// req 1.
+int[] duracaoCorridas = new int[6];
+
+for (int i = 0; i < duracaoCorridas.Length; i++)
+{
+  bool valida = false;
+
+  while (!valida)
+  {
+    Console.Write($"Duração da corrida {i + 1}: ");
+    if (int.TryParse(Console.ReadLine(), out duracaoCorridas[i]))
+      valida = true;
+    else
+      Console.WriteLine("Digite um número válido.");
+  }
+}
+
+// req 2.
+int totalMinutos = 0;
+int corridaMaisLonga = duracaoCorridas[0];
+int indiceCorridaMaisLonga = 0;
+
+for (int i = 0; i < duracaoCorridas.Length; i++)
+{
+  totalMinutos += duracaoCorridas[i];
+
+  if (duracaoCorridas[i] > corridaMaisLonga)
+  {
+    corridaMaisLonga = duracaoCorridas[i];
+    indiceCorridaMaisLonga = i;
+  }
+}
+
+// req 3.
+decimal mediaMinutos = (decimal)totalMinutos / duracaoCorridas.Length;
+
+// req 4.
+int qtdCorridasAcimaMedia = 0;
+
+foreach (int duracao in duracaoCorridas)
+{
+  if (duracao > mediaMinutos)
+    qtdCorridasAcimaMedia += 1;
+}
+
+// req 5.
+Console.WriteLine("\n=== RELATÓRIO ===");
+
+for (int i = 0; i < duracaoCorridas.Length; i++)
+  Console.WriteLine($"Corrida {i + 1}: {duracaoCorridas[i]} minutos");
+
+Console.WriteLine();
+Console.WriteLine(@$"Total: {totalMinutos} minutos
+
+Corrida mais longa (posição, duração): {indiceCorridaMaisLonga}, {corridaMaisLonga} minutos
+Média: {mediaMinutos:N1} minutos
+Quantidade de corridas acima da média: {qtdCorridasAcimaMedia}
+");
